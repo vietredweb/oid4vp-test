4d74a0436c69580c99ea6cb78c8fd4717ec75a6f
Viet <viet@r-digital.tech>
2025-08-01 16:46:43 +0700

    Update

--- pages/api/statement.js
+++ pages/api/statement.js
@@ -8,36 +8,43 @@ export default function handler(req, res) {
 
   res.end(
     JSON.stringify({
-      response_type: "vp_token",
-      client_id: "https://oid4vp-test.vercel.app",
-      redirect_uri: "https://oid4vp-test.vercel.app/callback",
-      scope: "openid",
-      state: "abc123",
-      nonce: "xyz789",
-      presentation_definition: {
-        id: "age-check-18",
-        input_descriptors: [
-          {
-            id: "age-verification",
-            name: "Verify age ≥ 18",
-            purpose: "Ensure the user is at least 18 years old",
-            constraints: {
-              fields: [
-                {
-                  path: [
-                    "$.credentialSubject.birthDate",
-                    "$.vc.credentialSubject.birthDate",
-                  ],
-                  filter: {
-                    type: "string",
-                    format: "date",
-                    maximum: "2007-08-01",
-                  },
+      authorization_request: {
+        response_type: "vp_token",
+        client_id: "https://oid4vp-test.vercel.app",
+        redirect_uri: "https://oid4vp-test.vercel.app/callback",
+        scope: "openid",
+        state: "abc123",
+        nonce: "xyz789",
+        presentation_definition: {
+          id: "age-check-18",
+          input_descriptors: [
+            {
+              id: "age-verification",
+              name: "Verify age ≥ 18",
+              purpose: "Ensure the user is at least 18 years old",
+              format: {
+                ldp_vc: {
+                  proof_type: ["Ed25519Signature2018", "Ed25519Signature2020"],
                 },
-              ],
+              },
+              constraints: {
+                fields: [
+                  {
+                    path: [
+                      "$.credentialSubject.birthDate",
+                      "$.vc.credentialSubject.birthDate",
+                    ],
+                    filter: {
+                      type: "string",
+                      format: "date",
+                      maximum: "2007-08-01",
+                    },
+                  },
+                ],
+              },
             },
-          },
-        ],
+          ],
+        },
       },
     })
   );
