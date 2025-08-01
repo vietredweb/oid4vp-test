823906bc2550f038a1ac14db40ca55382ab50ffe
Viet <viet@r-digital.tech>
2025-08-01 15:40:21 +0700

    Fix

--- pages/api/statement.js
+++ pages/api/statement.js
@@ -1,60 +1,67 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/oauth-authz-req+json");
+  res
+    .status(200)
+    .setHeader(
+      "Content-Type",
+      "application/oauth-authz-req+json; charset=UTF-8"
+    );
 
-  res.status(200).json({
-    response_type: "vp_token",
-    client_id: "https://oid4vp-test.vercel.app",
-    redirect_uri: "https://oid4vp-test.vercel.app/callback", // fake for now
-    scope: "openid",
-    state: "123456",
-    nonce: "abcdef",
-    presentation_definition: {
-      id: "age-country-check",
-      input_descriptors: [
-        {
-          id: "age_check",
-          name: "Age Verification",
-          purpose: "You must be 18+",
-          schema: [
-            {
-              uri: "https://schema.org/Person",
-            },
-          ],
-          constraints: {
-            fields: [
+  res.end(
+    JSON.stringify({
+      response_type: "vp_token",
+      client_id: "https://oid4vp-test.vercel.app",
+      redirect_uri: "https://oid4vp-test.vercel.app/callback", // fake for now
+      scope: "openid",
+      state: "123456",
+      nonce: "abcdef",
+      presentation_definition: {
+        id: "age-country-check",
+        input_descriptors: [
+          {
+            id: "age_check",
+            name: "Age Verification",
+            purpose: "You must be 18+",
+            schema: [
               {
-                path: ["$.birthDate"],
-                filter: {
-                  type: "string",
-                  format: "date",
-                  maximum: "2007-08-01",
-                },
+                uri: "https://schema.org/Person",
               },
             ],
-          },
-        },
-        {
-          id: "country_check",
-          name: "Country Verification",
-          purpose: "Must be from Germany",
-          schema: [
-            {
-              uri: "https://schema.org/Person",
-            },
-          ],
-          constraints: {
-            fields: [
-              {
-                path: ["$.address.addressCountry"],
-                filter: {
-                  type: "string",
-                  const: "DE",
+            constraints: {
+              fields: [
+                {
+                  path: ["$.birthDate"],
+                  filter: {
+                    type: "string",
+                    format: "date",
+                    maximum: "2007-08-01",
+                  },
                 },
+              ],
+            },
+          },
+          {
+            id: "country_check",
+            name: "Country Verification",
+            purpose: "Must be from Germany",
+            schema: [
+              {
+                uri: "https://schema.org/Person",
               },
             ],
+            constraints: {
+              fields: [
+                {
+                  path: ["$.address.addressCountry"],
+                  filter: {
+                    type: "string",
+                    const: "DE",
+                  },
+                },
+              ],
+            },
           },
-        },
-      ],
-    },
-  });
+        ],
+      },
+    })
+  );
 }
